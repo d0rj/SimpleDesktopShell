031cb9fd59ab5a40a031aba5976467e2fcbd60e4
d0rj <hanaan@internet.ru>
2021-10-12 22:28:15 +0300

    feat: add some new tweaks to disable

---
+++ Security/CommandLine.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+
+
+namespace SimpleDesktopShell.Security
+{
+	public class CommandLine
+	{
+		private const string RegisterKeyPath = @"Software\Policies\Microsoft\Windows";
+
+		public static void SetEnabled(bool isEnabled)
+		{
+			using RegistryKey objRegistryKey =
+				Registry.CurrentUser.CreateSubKey(RegisterKeyPath);
+
+			if (isEnabled && objRegistryKey.GetValue("DisableCMD") != null)
+			{
+				objRegistryKey.DeleteValue("DisableCMD");
+			}
+			else
+			{
+				objRegistryKey.SetValue("DisableCMD", "2");
+			}
+		}
+	}
+}
---
+++ Security/WindowsButton.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDesktopShell.Security
+{
+	public class WindowsButton
+	{
+		private const string RegisterKeyPath = @"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer";
+
+		public static void SetEnabled(bool isEnabled)
+		{
+
+			using RegistryKey localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+
+			var reg = localMachine.OpenSubKey(RegisterKeyPath, true);
+			if (reg == null)
+			{
+				reg = localMachine.CreateSubKey(RegisterKeyPath);
+			}
+			reg.Dispose();
+
+			using RegistryKey objRegistryKey =
+				Registry.CurrentUser.CreateSubKey(RegisterKeyPath);
+			if (isEnabled && objRegistryKey.GetValue("NoWinKeys") != null)
+			{
+				objRegistryKey.DeleteValue("NoWinKeys");
+			}
+			else
+			{
+				objRegistryKey.SetValue("NoWinKeys", "1");
+			}
+		}
+	}
+}
