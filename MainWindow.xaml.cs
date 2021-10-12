936d40646fb5077b3d41090516c307a162c090e8
d0rj <hanaan@internet.ru>
2021-10-12 14:13:21 +0300

    Initial commit
    Can handle only 'Alt+F4'.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace SimpleDesktopShell
+{
+	public sealed partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			PreviewKeyDown += (object sender, KeyEventArgs e) =>
+			{
+				if (e.Key == Key.Escape)
+				{
+					Close();
+				}
+				if (e.Key == Key.System && e.SystemKey == Key.F4)
+				{
+					e.Handled = true;
+				}
+			};
+		}
+	}
+}
